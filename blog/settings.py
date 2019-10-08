29673e4825d5ff3908b90814ac9fe8d5e53a72f8
SHANKAR DAKOLIA <shankardakolia@gmail.com>
2019-10-09 01:24:26 +0530

    Final Deploy with DB issue resolved

--- blog/settings.py
+++ blog/settings.py
@@ -78,9 +78,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'CMS',
+        'USER': 'registry',
+        'PASSWORD': 'Admin@19741988',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
+
 }
 
 
